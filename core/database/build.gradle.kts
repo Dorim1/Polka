33e2cdb87a45865c0600f9621abdcfd324f3849c
Dreamy <tolya.lyashhenko.05@mail.ru>
2026-07-13 15:53:40 +0300

    created the core:database empty module

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -1,12 +1,13 @@
 plugins {
     alias(libs.plugins.polka.android.library)
+    alias(libs.plugins.polka.android.room)
     alias(libs.plugins.polka.hilt)
 }
 
 android {
-    namespace = "ru.anlyashenko.core.data"
+    namespace = "ru.anlyashenko.core.database"
 }
 
 dependencies {
-    api(projects.core.database)
+
 }
